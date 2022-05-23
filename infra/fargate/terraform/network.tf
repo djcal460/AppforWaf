7bcd7e6c6bd903b16f07193101a0b1148c70bb7d
Derek Calzadillas <derek_calzadillas@comcast.com>
2022-05-23 15:43:39 -0400

    tgw test

--- infra/fargate/terraform/network.tf
+++ infra/fargate/terraform/network.tf
@@ -1,15 +1,40 @@
 data "aws_vpc" "selected"{
   id = var.vpc_id
 }
-data "aws_internet_gateway" "IGW" {
-  filter {
-    name   = "attachment.vpc-id"
-    values = [data.aws_vpc.selected.id]
+
+data "aws_subnets" "public" {
+
+  tags = {
+    Type = "Public"
+  }
+}
+resource "aws_ec2_transit_gateway" "my-test-tgw" {
+  description                     = "my-test-transit-gateway"
+  amazon_side_asn                 = 64512
+  auto_accept_shared_attachments  = "disable"
+  default_route_table_association = "enable"
+  default_route_table_propagation = "enable"
+  dns_support                     = "enable"
+  vpn_ecmp_support                = "enable"
+  tags {
+    Name = "my-test-transit-gateway"
+  }
+}
+
+resource "aws_ec2_transit_gateway_vpc_attachment" "my-test-transit-gateway-attachment" {
+  transit_gateway_id = "${aws_ec2_transit_gateway.my-test-tgw.id}"
+  vpc_id             = "${var.vpc_id}"
+  dns_support        = "enable"
+
+  subnet_ids = [data.aws_subnets.public.ids]
+
+  tags {
+    Name = "my-test-tgw-vpc-attachment"
   }
 }
 resource "aws_route" "route" {
   count                  = var.rt_non_prod ? 1 : 0 
-  gateway_id             = data.aws_internet_gateway.IGW.id
+  gateway_id             = data.aws_ec2_transit_gateway.my-test-tgw.id
   route_table_id         = data.aws_vpc.selected.main_route_table_id
   destination_cidr_block = var.routetable_main_cidr_igw
 }
